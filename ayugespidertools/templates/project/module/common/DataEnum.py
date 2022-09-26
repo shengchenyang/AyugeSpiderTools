cf183f81d4ec1b306bf0f8a4a5fad8a93d6320e3
shengchenyang <15538221825@163.com>
2022-09-26 17:33:21 +0800

    AyugeSpiderTools - 修改 dataEnum 文件

--- ayugespidertools/templates/project/module/common/DataEnum.py
+++ ayugespidertools/templates/project/module/common/DataEnum.py
@@ -5,11 +5,7 @@ from enum import Enum
 
 class TableEnum(Enum):
     """
-    数据库表枚举信息
+    数据库表枚举信息示例
     """
 
-    # 文章列表信息
     aritle_list_table = {"value": "article_info_list", "notes": "项目列表信息", "demand_code": "DemoSpider_aritle_list_table_demand_code"}
-
-    # 小说列表信息
-    book_info_list_table = {"value": "book_info_list", "notes": "小说列表信息", "demand_code": "DemoSpider_book_info_list_table_demand_code"}
