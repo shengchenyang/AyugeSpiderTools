afd5a3e5996de40fa6e6ff524347670bdd7c1ab8
shengchenyang <15538221825@163.com>
2023-11-12 07:06:09 +0800

    update aiohttp_request test

--- tests/test_request.py
+++ tests/test_request.py
@@ -6,16 +6,30 @@ def callback_tmp(response):
 
 
 def test_aiohttp_reqeust():
+    _url = "https://www.baidu.com"
+    _ua = "Opera/9.80 (X11; Linux i686|; Ubuntu/14.10) Presto/2.12.388 Version/12.16"
+    _ck = {"name": "ayuge"}
+    _meta = {
+        "curr_page": 1,
+    }
     r = AiohttpRequest(
-        url="https://www.baidu.com",
+        url=_url,
         callback=callback_tmp,
         method="GET",
         headers={
-            "USER_AGENT": "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16",
+            "USER_AGENT": _ua,
         },
-        cookies={"name": "ayuge"},
-        meta={
-            "curr_page": 1,
-        },
+        cookies=_ck,
+        meta=_meta,
     )
-    assert r is not None
+    assert all(
+        [
+            r.url == _url,
+            r.method == "GET",
+            r.headers["USER_AGENT"] == bytes(_ua, encoding="utf-8"),
+            r.cookies == _ck,
+            r.meta == {"curr_page": 1, "aiohttp": {"args": {"url": _url}}},
+        ]
+    )
+
+    # todo: 添加具体应用场景及更详细的的测试用例。
