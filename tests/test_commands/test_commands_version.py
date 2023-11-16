4456192a4e7701b92f54a28bffddb069e3bbff17
shengchenyang <15538221825@163.com>
2023-11-16 11:06:30 +0800

    optimization: version 测试更合理

--- tests/test_commands/test_commands_version.py
+++ tests/test_commands/test_commands_version.py
@@ -1,11 +1,14 @@
 import sys
 from io import StringIO
+from pathlib import Path
 
+import toml
 from scrapy.utils.testproc import ProcessTest
 from twisted.internet import defer
 from twisted.trial import unittest
 
 from ayugespidertools.commands.version import AyuCommand
+from ayugespidertools.config import NormalConfig
 
 ProcessTest.prefix = [sys.executable, "-m", "ayugespidertools.utils.cmdline"]
 
@@ -20,11 +23,17 @@ def test_version():
 class VersionTest(ProcessTest, unittest.TestCase):
     command = "version"
 
+    @staticmethod
+    def _version() -> str:
+        toml_file = Path(NormalConfig.ROOT_DIR, "pyproject.toml")
+        conf = toml.load(toml_file)
+        return conf["tool"]["poetry"]["version"]
+
     @defer.inlineCallbacks
     def test_output(self):
         encoding = getattr(sys.stdout, "encoding") or "utf-8"
         _, out, _ = yield self.execute([])
         self.assertEqual(
             out.strip().decode(encoding),
-            "AyugeSpiderTools 3.6.1",
+            f"AyugeSpiderTools {self._version()}",
         )
