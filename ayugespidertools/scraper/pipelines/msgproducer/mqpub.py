ac54fd0a7611a8e63b46689da83718a9cebdb013
shengchenyang <15538221825@163.com>
2024-01-17 15:22:41 +0800

    fix: add mq connection close func

--- ayugespidertools/scraper/pipelines/msgproducer/mqpub.py
+++ ayugespidertools/scraper/pipelines/msgproducer/mqpub.py
@@ -1,5 +1,5 @@
 import json
-from typing import Union
+from typing import TYPE_CHECKING, Union
 
 import pika
 
@@ -10,28 +10,32 @@ __all__ = [
     "AyuMQPipeline",
 ]
 
+if TYPE_CHECKING:
+    from pika.adapters.blocking_connection import BlockingChannel, BlockingConnection
+
+    from ayugespidertools.common.typevars import MQConf
+
 
 class AyuMQPipeline:
-    """消息队列发布场景的 scrapy pipeline 扩展 - pika mq"""
+    channel: "BlockingChannel"
+    conn: "BlockingConnection"
 
-    def __init__(self):
-        self.channel = None
-
-    def _dict_to_bytes(self, item: Union[AyuItem, dict]) -> bytes:
+    @staticmethod
+    def _dict_to_bytes(item: Union[AyuItem, dict]) -> bytes:
         item_dict = ReuseOperation.item_to_dict(item)
         item_json_str = json.dumps(item_dict)
         return bytes(item_json_str, encoding="utf-8")
 
     def open_spider(self, spider):
         assert hasattr(spider, "rabbitmq_conf"), "未配置 RabbitMQ 连接信息！"
-        _mq_conf = spider.rabbitmq_conf
+        _mq_conf: "MQConf" = spider.rabbitmq_conf
         mq_conn_param = pika.URLParameters(
             f"amqp://{_mq_conf.username}:{_mq_conf.password}"
             f"@{_mq_conf.host}:{_mq_conf.port}/{_mq_conf.virtualhost}"
             f"?heartbeat={_mq_conf.heartbeat}&socket_timeout={_mq_conf.socket_timeout}"
         )
-        conn = pika.BlockingConnection(parameters=mq_conn_param)
-        self.channel = conn.channel()
+        self.conn = pika.BlockingConnection(parameters=mq_conn_param)
+        self.channel = self.conn.channel()
         self.channel.queue_declare(
             queue=_mq_conf.queue,
             durable=_mq_conf.durable,
@@ -41,7 +45,7 @@ class AyuMQPipeline:
         self.channel.confirm_delivery()
 
     def close_spider(self, spider):
-        pass
+        self.conn.close()
 
     def process_item(self, item, spider):
         self.channel.basic_publish(
