9e35b6334a70d785789f3ea6ddd031f75d68d255
shengchenyang <15538221825@163.com>
2024-01-30 10:59:35 +0800

    chore: add oss dependency(#16)

--- ayugespidertools/scraper/pipelines/__init__.py
+++ ayugespidertools/scraper/pipelines/__init__.py
@@ -17,6 +17,7 @@ from ayugespidertools.scraper.pipelines.mysql.turbo import AyuTurboMysqlPipeline
 from ayugespidertools.scraper.pipelines.mysql.twisted import AyuTwistedMysqlPipeline
 from ayugespidertools.scraper.pipelines.oracle.fantasy import AyuFtyOraclePipeline
 from ayugespidertools.scraper.pipelines.oracle.twisted import AyuTwistedOraclePipeline
+from ayugespidertools.scraper.pipelines.oss.ali import AyuAsyncOssPipeline
 from ayugespidertools.scraper.pipelines.postgres.asynced import AyuAsyncPostgresPipeline
 from ayugespidertools.scraper.pipelines.postgres.fantasy import AyuFtyPostgresPipeline
 from ayugespidertools.scraper.pipelines.postgres.twisted import (
@@ -39,6 +40,7 @@ __all__ = [
     "AyuTwistedMysqlPipeline",
     "AyuFtyOraclePipeline",
     "AyuTwistedOraclePipeline",
+    "AyuAsyncOssPipeline",
     "AyuAsyncPostgresPipeline",
     "AyuFtyPostgresPipeline",
     "AyuTwistedPostgresPipeline",
