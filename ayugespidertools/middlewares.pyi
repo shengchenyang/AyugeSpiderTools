ff2d2d4cdf3f0ffe84f515024d87af86986d541f
shengchenyang <15538221825@163.com>
2026-06-08 00:11:50 +0800

    chore: add middlewares pyi

--- ayugespidertools/middlewares.pyi
+++ ayugespidertools/middlewares.pyi
@@ -4,12 +4,16 @@ from ayugespidertools.scraper.downloadermiddlewares.headers.ua import (
 from ayugespidertools.scraper.downloadermiddlewares.netlib.aiohttplib import (
     AiohttpDownloaderMiddleware,
 )
+from ayugespidertools.scraper.downloadermiddlewares.netlib.curlcffi import (
+    CurlCffiDownloaderMiddleware,
+)
 from ayugespidertools.scraper.downloadermiddlewares.proxy.default import (
     ProxyDownloaderMiddleware,
 )
 
 __all__ = [
     "AiohttpDownloaderMiddleware",
+    "CurlCffiDownloaderMiddleware",
     "ProxyDownloaderMiddleware",
     "RandomRequestUaMiddleware",
 ]
