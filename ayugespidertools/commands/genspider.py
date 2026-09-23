094930846059ec35648f45b12d54dd8951cff7da
ayuge <15538221825@163.com>
2026-09-24 03:12:36 +0800

    feat: add custom spider templates (#41)
    Co-authored-by: shengchenyang <ayugesheng@gmail.com>

--- ayugespidertools/commands/genspider.py
+++ ayugespidertools/commands/genspider.py
@@ -9,9 +9,7 @@ class AyuCommand(Command):
     @property
     def templates_dir(self) -> str:
         assert self.settings is not None
-        return str(
-            Path(
-                Path(ayugespidertools.__path__[0], "templates"),
-                "spiders",
-            )
-        )
+        templates_dir = self.settings["TEMPLATES_DIR"]
+        if self.settings.getpriority("TEMPLATES_DIR") == 0:
+            templates_dir = Path(ayugespidertools.__path__[0], "templates")
+        return str(Path(templates_dir, "spiders"))
